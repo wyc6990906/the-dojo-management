fce30852af09272e1cafb7699c39dbe1633cac2c
Dylan Wang <wyc6990906@hotmail.com>
2022-09-28 16:43:30 -0600

    log user out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {useLogout} from "../hooks/useLogout";
 import "./Navbar.css"
 import Temple from "../assets/temple.svg"
 
 const Navbar = () => {
+  const {logout, isPending} = useLogout()
+
   return (
     <div className="navbar">
       <ul>
@@ -15,7 +18,8 @@ const Navbar = () => {
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">Signup</Link></li>
         <li>
-          <button className="btn">Logout</button>
+          {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+          {isPending && <button className="btn" disabled>Logging out...</button>}
         </li>
       </ul>
     </div>
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,31 +1,34 @@
-import { useEffect, useState } from 'react'
-import { projectAuth } from '../firebase/config'
-import { useAuthContext } from './useAuthContext'
+import {useEffect, useState} from 'react'
+import {projectAuth, projectFirestore} from '../firebase/config'
+import {useAuthContext} from './useAuthContext'
 
 export const useLogout = () => {
   const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
-  
+  const {dispatch} = useAuthContext()
+
   const logout = async () => {
     setError(null)
     setIsPending(true)
 
     try {
+      // update online status
+      const {uid} = projectAuth.currentUser
+      await projectFirestore.collection('users').doc(uid).update({online: false})
+
       // sign the user out
       await projectAuth.signOut()
-      
+
       // dispatch logout action
-      dispatch({ type: 'LOGOUT' })
+      dispatch({type: 'LOGOUT'})
 
       // update state
       if (!isCancelled) {
         setIsPending(false)
         setError(null)
-      } 
-    } 
-    catch(err) {
+      }
+    } catch (err) {
       if (!isCancelled) {
         setError(err.message)
         setIsPending(false)
@@ -37,5 +40,5 @@ export const useLogout = () => {
     return () => setIsCancelled(true)
   }, [])
 
-  return { logout, error, isPending }
+  return {logout, error, isPending}
 }
