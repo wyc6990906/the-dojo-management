de9841b07b6cb4c4cf64f164ad963b81f2d0890a
Dylan Wang <wyc6990906@hotmail.com>
2022-09-29 20:56:44 -0600

    avatar components working

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./Avatar.css"
+
+const Avatar = ({src}) => {
+  return (
+    <div className="avatar">
+      <img src={src} alt="user avatar"/>
+    </div>
+  );
+};
+
+export default Avatar;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,15 +3,18 @@ import {NavLink} from "react-router-dom";
 import "./Sidebar.css"
 import DashboardIcon from "../assets/dashboard_icon.svg"
 import AddIcon from "../assets/add_icon.svg"
-
+import Avatar from "./Avatar";
+import {useAuthContext} from "../hooks/useAuthContext";
 
 const Sidebar = () => {
+  const {user} = useAuthContext()
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
           {/* avatar and username here later  */}
-          <p>Welcome User</p>
+          <Avatar src={user.photoURL}/>
+          <p>Welcome {user.displayName}</p>
         </div>
         <nav className="links">
           <ul>
