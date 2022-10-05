7765c7aeeac58ba969070b80ddf833191d604601
Dylan Wang <wyc6990906@hotmail.com>
2022-10-05 16:28:27 -0600

    making a project grid

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import Avatar from "./Avatar";
 
 import "./ProjectList.css"
 
 const ProjectList = ({projects}) => {
   return (
-    <div>
+    <div className="project-list" >
       {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map(project => (
-        <div key={project.id}>{project.name}</div>
+        <Link key={project.id} to={`/project/${project.id}`}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL}/>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+        </Link>
       ))}
     </div>
   );
