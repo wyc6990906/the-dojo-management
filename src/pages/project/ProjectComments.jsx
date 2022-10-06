a60889a2517174c8ff985f7df77e4236fea46e7b
Dylan Wang <wyc6990906@hotmail.com>
2022-10-06 17:45:22 -0600

    listing comments

--- src/pages/project/ProjectComments.jsx
+++ src/pages/project/ProjectComments.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react"
-import { timestamp } from "../../firebase/config"
-import { useAuthContext } from "../../hooks/useAuthContext"
-import { useFirestore } from "../../hooks/useFirestore"
+import {useState} from "react"
+import {timestamp} from "../../firebase/config"
+import {useAuthContext} from "../../hooks/useAuthContext"
+import {useFirestore} from "../../hooks/useFirestore"
+import Avatar from "../../components/Avatar";
 
-export default function ProjectComments({ project }) {
-  const { user } = useAuthContext()
-  const { updateDocument, response } = useFirestore('projects')
+export default function ProjectComments({project}) {
+  const {user} = useAuthContext()
+  const {updateDocument, response} = useFirestore('projects')
   const [newComment, setNewComment] = useState('')
 
   const handleSubmit = async (e) => {
@@ -30,6 +31,22 @@ export default function ProjectComments({ project }) {
   return (
     <div className="project-comments">
       <h4>Project Comments</h4>
+      <ul>
+        {project.comments.length > 0 && project.comments.map(comment => (
+          <li key={comment.id}>
+            <div className="comment-author">
+              <Avatar src={comment.photoURL} />
+              <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>date here</p>
+            </div>
+            <div className="comment-content">
+              <p>{comment.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
 
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
