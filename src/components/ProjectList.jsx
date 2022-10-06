00ae8a2bc4f70d3db040870b84638934f4018604
Dylan Wang <wyc6990906@hotmail.com>
2022-10-05 18:16:05 -0600

    fetching a single project detail

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -9,7 +9,7 @@ const ProjectList = ({projects}) => {
     <div className="project-list" >
       {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map(project => (
-        <Link key={project.id} to={`/project/${project.id}`}>
+        <Link key={project.id} to={`/projects/${project.id}`}>
           <h4>{project.name}</h4>
           <p>Due by {project.dueDate.toDate().toDateString()}</p>
           <div className="assigned-to">
